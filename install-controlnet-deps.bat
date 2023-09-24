c9aaab535590c0f1e8411ca619519053ba2e4f13
David Landeros <david.landeros@digitalvaultstudio.com>
2023-09-24 00:51:19 -0600

    update install scripts

--- install-controlnet-deps.bat
+++
@@ -1,7 +0,0 @@
-
-set pythonpath=%~dp0\venv\Scripts\python.exe
-%pythonpath% -m ensurepip
-%pythonpath% -m pip install mediapipe
-%pythonpath% -m pip install svglib
-%pythonpath% -m pip install fvcore
-%pythonpath% -m pip install opencv-python>=4.8.0
---
+++ install-deps-and-expand-archives.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo checking out sd-webui-controlnet
+pushd %~dp0
+cd extensions
+git clone https://github.com/Mikubill/sd-webui-controlnet.git
+cd sd-webui-controlnet
+git checkout 54f7c64b4dc4a4206e76a152651d5f0b0fac248f
+popd
+
+echo creating venv..
+if not exist %~dp0venv\Scripts\python.exe (
+    python -m venv %~dp0venv
+)
+
+echo installing dependencies..
+%~dp0venv\Scripts\python.exe -m ensurepip
+%~dp0venv\Scripts\python.exe -m pip install mediapipe
+%~dp0venv\Scripts\python.exe -m pip install svglib
+%~dp0venv\Scripts\python.exe -m pip install fvcore
+%~dp0venv\Scripts\python.exe -m pip install opencv-python
+
+echo expanding archives..
+%~dp0venv\Scripts\python.exe %~dp0archives-for-controlnet\expand.py
+%~dp0venv\Scripts\python.exe %~dp0archives-for-stable-difussion\expand.py
